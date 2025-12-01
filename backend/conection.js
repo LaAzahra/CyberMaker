0f0f6ea6626308631bad435f026180c5a1d8652a
LaAzahra <lalla.rachid22@gmail.com>
2025-11-30 21:43:24 -0300

    commit

--- backend/conection.js
+++ backend/conection.js
@@ -3,9 +3,13 @@ import mysql from "mysql2/promise";
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: Number(process.env.DB_PORT)
 });
+
+pool.getConnection()
+  .then(() => console.log("✅ MySQL conectado"))
+  .catch(err => console.error("❌ Erro MySQL:", err.message));
 
 export default pool;
